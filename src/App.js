73e47f8d91a52a64706063e926123a5706966b3d
Minh <minhhoangdev3108@gmail.com>
2024-07-15 00:38:28 +0700

    feat: load data in page product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/login/Login";
+import Signup from "./components/login/Signup";
+import Logout from "./components/login/Logout";
+import Profile from "./components/login/Profile";
+import Products from "./components/Products";
+import Dashboard from "./components/admin/DashBoard";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import DetailProduct from "./components/DetailProduct";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/xemchitiet/:phoneId" element={<DetailProduct />} />
+        <Route path="/categorymanagement/:option" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = ()=>{
+    return(
+        <div className="footer">
+            <p>Đỗ Đức Thiện</p>
+            <p> Thông tin liên hệ: 0979210138</p>  
+            <p>Rất vui lòng khi được phục vụ quý khách</p>
+        </div>
+    )
+}
+export default Footer;
